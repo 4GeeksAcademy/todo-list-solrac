175543fa343d202ea5aef9c3589f7677ab71cc17
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2025-07-28 18:46:49 +0000

    avance de la lógica

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+	const [inputData, setInputData] = useState("");
+	const [task, setTask] = useState(null);
+
+
+	const onInputChange = (event) => {
+		setInputData(event.target.value)
+	}
+
+	const handleKeyUp = (event) => {
+		if (event.key === "Enter") {
+			setTask(inputData)
+			setInputData("")
+		}
+
+	}
+
+	const addTask = (event) => {
+
+	}
+
+
+	return (
+
+		<div className="task m-5 p-2 bg-warning">
+			<div className="text-center">
+				<h1 className="display-2">TODO-LIST</h1>
+			</div>
+			<div className="task-body m-2">
+				<label className="form-label" htmlFor="taskNew"><strong>New Task:</strong></label>
+				<input className="form-control"
+					id="taskNew"
+					placeholder="Add Task"
+					type="text"
+					value={inputData}
+					onChange={onInputChange}
+					onKeyUp={handleKeyUp} />
+			</div>
+			<div className="row">
+				<p className="text-center">{task}</p>
+			</div>
 		</div>
+
+
 	);
 };
 
